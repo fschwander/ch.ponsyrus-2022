b0d7c7a43f4a053d7631cceb984938e4208f12d9
FabianSchwander <fabian.schwander@students.fhnw.ch>
2022-05-26 17:10:54 +0200

    extracted css and clean-up

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,20 @@ import addressImage from './res/imgs/addressImage.svg'
 function App() {
   return (
     <div className="App">
-      <div className="App">
-        <div className="contentContainer">
-          <div className="eventContainer borderContainer">
-            <p>Kleingenossenschaftliches Wohnen seit 1984.</p>
-            <p>Kontakt: <a href="mailto:genossenschaft@ponsyrus.ch">genossenschaft@ponsyrus.ch</a></p>
-          </div>
-
-          <div className="addressContainer">
-            <h1>Anwandstrasse 9<br/>8004 Zürich</h1>
-          </div>
+      <div className="contentContainer">
+        <div className="eventContainer borderContainer">
+          <p>Kleingenossenschaftliches Wohnen seit 1984.</p>
+          <p>Kontakt: <a href="mailto:genossenschaft@ponsyrus.ch">genossenschaft@ponsyrus.ch</a></p>
         </div>
 
-        <div className="backgroundContainer">
-          <img alt="adresse" className="addressImage" src={ addressImage }/>
-          <img alt="ponsyrus" className="houseImage" src={ houseImage }/>
+        <div className="addressContainer">
+          <h1>Anwandstrasse 9<br/>8004 Zürich</h1>
         </div>
+      </div>
+
+      <div className="backgroundContainer">
+        <img alt="adresse" className="addressImage" src={addressImage}/>
+        <img alt="ponsyrus" className="houseImage" src={houseImage}/>
       </div>
     </div>
   );
