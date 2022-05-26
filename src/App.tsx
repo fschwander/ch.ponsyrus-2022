dd60f1c4dd3ede40f10625ab784ff83ed86893b9
FabianSchwander <fabian.schwander@students.fhnw.ch>
2022-05-26 12:42:49 +0200

    moved all old project files to new ts project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import houseImage from './res/imgs/houseImage.svg'
+import addressImage from './res/imgs/addressImage.svg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+        <div className="contentContainer">
+          <div className="eventContainer borderContainer">
+            <p>Kleingenossenschaftliches Wohnen seit 1984.</p>
+            <p>Kontakt: <a href="mailto:genossenschaft@ponsyrus.ch">genossenschaft@ponsyrus.ch</a></p>
+          </div>
+
+          <div className="addressContainer">
+            <h1>Anwandstrasse 9<br/>8004 Zürich</h1>
+          </div>
+        </div>
+
+        <div className="backgroundContainer">
+          <img className="addressImage" src={ addressImage }/>
+          <img className="houseImage" src={ houseImage }/>
+        </div>
+      </div>
     </div>
   );
 }
